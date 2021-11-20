d28e1b0c717d0845852ea3227683667b0a84d0cf
[Shweta Mhetre] <[mhetreshweta16@gmail.com]>
2021-11-20 14:30:35 +0530

    [shweta] added the code of cab invoice problem

---
+++ CabInvoiceGeneratorProject/InvoiceSummary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGeneratorProject
+{
+    public class InvoiceSummary
+    {
+        private int numberOfRides;
+        private double totalFare;
+        private double averageFare;
+
+        public InvoiceSummary(int numberOfRides, double totalFare)
+        {
+            this.numberOfRides = numberOfRides;
+            this.totalFare = totalFare;
+            this.averageFare = totalFare / numberOfRides;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (!(obj is InvoiceSummary))
+            {
+                return false;
+            }
+            InvoiceSummary summary = (InvoiceSummary)obj;
+            return this.numberOfRides == summary.numberOfRides && this.totalFare == summary.totalFare && this.averageFare == summary.averageFare;
+
+        }
+    }
+}
---
+++ CabInvoiceTesting/UnitTest1.cs
@@ -0,0 +1,38 @@
+using CabInvoiceGeneratorProject;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CabInvoiceTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenDistanceAndTimeShouldReturnTotalFare()
+        {
+            double expected = 25;
+            double distance = 2.0;
+            int time = 5;
+
+            InvoiceGenerator invoice = new InvoiceGenerator(RideType.NORMAL);
+            
+            double fare = invoice.calculateFare(distance, time);
+            
+            Assert.AreEqual(expected, fare);
+        }
+
+        [TestMethod]
+        public void GivenMultipleRidesReturnTotalFare()
+        {
+            
+
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.PREMIUM);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+            InvoiceSummary expected = new InvoiceSummary(rides.Length,60);
+
+            InvoiceSummary actual = invoiceGenerator.CalculateFare(rides);
+            Assert.AreEqual(actual, expected);
+
+ 
+        }
+    }
+}
